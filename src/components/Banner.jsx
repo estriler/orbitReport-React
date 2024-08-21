efbf7221d6b969b098667e4c172d577518260be2
Eric S <estriler@hotmail.com>
2024-08-20 21:06:47 -0500

    added banner component

---
+++ src/components/Banner.jsx
@@ -0,0 +1,8 @@
+export default function Banner () {
+    return (
+        <div>
+            <header>Orbit Report</header>
+            <p>Click the buttons to see the satellites in that orbit type</p>
+        </div>
+    )
+}
